af6c42ff6a42a6e574ac071f9ed74ce44227fbae
zzm <641945043@qq.com>
2025-06-07 11:12:02 +0800

    更新README.md

--- README.md
+++ README.md
@@ -1,18 +1,34 @@
-# 位置获取工具
+# WhereAmI CLI
 
 这是一个简单的命令行工具，用于获取当前用户的经纬度位置信息。
 
 ## 安装
 
+### 从PyPI安装
+```bash
+pip install whereami-cli
+```
+
+### 从源码安装
 1. 确保您已安装Python 3.6或更高版本
-2. 安装依赖：
+2. 克隆仓库：
+```bash
+git clone https://github.com/zhangzimin/geo-location.git
+cd geo-location
+```
+3. 安装依赖：
 ```bash
 pip install -r requirements.txt
 ```
 
 ## 使用方法
 
-直接运行Python脚本：
+安装后，您可以直接使用命令：
+```bash
+whereami
+```
+
+或者运行Python脚本：
 ```bash
 python get_location.py
 ```
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="whereami-cli",
+    version="0.1.0",
+    author="ZhangZimin",
+    author_email="641945043@qq.com",
+    description="用于获取当前用户的经纬度位置信息的命令行工具",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zhangzimin/geo-location",
+    packages=find_packages(),
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "geocoder>=1.38.1",
+        "rich>=13.7.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "whereami=get_location:get_current_location",
+        ],
+    },
+) 
